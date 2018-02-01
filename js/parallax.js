349d28318d8078d5da2a070fc0cda00d1040abad
LAPTOP-LE5K4H75\mathi <mathias.vansant@immalle.be>
2018-02-01 21:53:36 +0100

    opmaak updated
    - voorbeelden zijn nu menu items
    - grijs/blawe kaders zijn nu grijs/groene kaders
    - grote knop op mainpage gaan naar menus.html
    - parallax fixed: 3 verschillende snelheden voor de 3 grijs/groene kaders
    - foto's bij grijs/groene kaders staan nu meer naar rechts (nu zijn ze dus ook effectief zichtbaar)
    - logo verwijders op mainpage
    - 'navigation-bar' verwijderd: niet meer nodig, tenzij ik met "ID's" ga werken?
    - footer aangepast
    
    TO DO:
    - 'facebook' in footer moet nog witte letterkleur
    - lettertypes overal OK?
    - menus.html (alles)

---
+++ js/parallax.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+    parallax();
+})
+
+function parallax() {
+    
+    var wScroll = $(window).scrollTop();
+    
+    $('.parallax--box1').css('top', -5+(wScroll*0.005)+'em')
+    $('.parallax--box2').css('top', -3+(wScroll*0.003)+'em')
+    $('.parallax--box3').css('top', -1+(wScroll*0.001)+'em')
+    
+}
