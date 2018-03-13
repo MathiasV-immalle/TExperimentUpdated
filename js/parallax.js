24523f9eca2be6d07e0d1d630cb1223035ee2f3b
LAPTOP-LE5K4H75\mathi <mathias.vansant@immalle.be>
2018-03-13 21:02:35 +0100

    upd menu add pics del navbar
    - nav removed
    - menus.html removed
    - favicon added
    - new pictures added
    - menus added to index
    - added menu css
    - upd titles
    
    only wallpaper needs a upd and menu's should be added.

--- js/parallax.js
+++ js/parallax.js
@@ -8,6 +8,6 @@ function parallax() {
     
     $('.parallax--box1').css('top', -5+(wScroll*0.005)+'em')
     $('.parallax--box2').css('top', -3+(wScroll*0.003)+'em')
-    $('.parallax--box3').css('top', -1+(wScroll*0.001)+'em')
+    $('.parallax--box3').css('top', -4+(wScroll*0.001)+'em')
     
 }
